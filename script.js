83c674d32606c0a7a5174dd6d73f13281ae2732a
cristina1p <c1plamadeala@gmail.com>
2024-08-12 20:53:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,27 +2,22 @@
 
 // Varianta 1
 
-let weightVadim = 78;
-let heightVadim = 1.69;
-let weightAlex = 92;
-let heightAlex = 1.95;
-let bmiVadim;
-let bmiAlex;
+let weightVadim1 = 78;
+let heightVadim1 = 1.69;
+let weightAlex1 = 92;
+let heightAlex1 = 1.95;
+let bmiVadim1;
+let bmiAlex1;
 
-// calculam mbi
+bmiVadim1 = weightVadim1 / (heightVadim1 ** 2);
+bmiAlex1 = weightAlex1 / (heightAlex1 ** 2);
 
-bmiVadim = (weightVadim / heightVadim) ** 2;
-bmiAlex = (weightAlex / heightAlex) ** 2;
+console.log(`BMI pentru Vadim este: ${bmiVadim1}.`);
+console.log(`BMI pentru Alex este: ${bmiAlex1}.`);
 
-let bmiVadimFinal = 'BMI a lui Vadim: ' + bmiVadim + '.'
-console.log(bmiVadimFinal);
+let markHigherBM1 = bmiVadim1 > bmiAlex1;
 
-let bmiAlexFinal = 'BMI a lui Alex: ' + bmiAlex + '.'
-console.log(bmiAlexFinal);
-
-// higher mbi Vadim
-
-let markHigherBMI = console.log(bmiVadimFinal > bmiAlexFinal);
+console.log(`BMI-ul pentru Vadim este mai mare decat pentru Alex: ${markHigherBM1}`)
 
 // Varianta 2 
 
@@ -33,18 +28,13 @@ let heightAlex2 = 1.76;
 let bmiVadim2;
 let bmiAlex2;
 
-// calculam mbi
+bmiVadim2 = weightVadim2 / (heightVadim2 ** 2);
+bmiAlex2 = weightAlex2 / (heightAlex2 ** 2);
 
-bmiVadim2 = (weightVadim2 / heightVadim2) ** 2;
-bmiAlex2 = (weightAlex2 / heightAlex2) ** 2;
+console.log(`BMI pentru Vadim este: ${bmiVadim2}.`);
+console.log(`BMI pentru Alex este: ${bmiAlex2}.`);
 
-let bmiVadimFinal2 = 'BMI a lui Vadim: ' + bmiVadim2 + '.'
-console.log(bmiVadimFinal2);
+let markHigherBM2 = bmiVadim2 > bmiAlex2;
 
-let bmiAlexFinal2 = 'BMI a lui Alex: ' + bmiAlex2 + '.'
-console.log(bmiAlexFinal2);
-
-// higher mbi Vadim
-
-let markHigherBMI2 = console.log(bmiVadimFinal2 > bmiAlexFinal2);
+console.log(`BMI-ul pentru Vadim este mai mare decat pentru Alex: ${markHigherBM2}`)
 
